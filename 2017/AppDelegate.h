065b1efb28319c3855e0bfd142c2613dd4908e7b
sreeku44 <2sreeku44@gmail.com>
2017-02-08 12:11:54 -0600

    Initial Commit

---
+++ 2017/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2017
+//
+//  Created by Sreekala Santhakumari on 2/8/17.
+//  Copyright © 2017 Klas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2017/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2017
+//
+//  Created by Sreekala Santhakumari on 2/8/17.
+//  Copyright © 2017 Klas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
